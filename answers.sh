04a535a3fbcd24830c5d3a8d514be358a9f2b4bd
Christy Smith <smithc@amc-tesla.cluster.pvt>
2017-02-19 12:12:23 -0700

    add new files

---
+++ answers.sh
@@ -0,0 +1,68 @@
+ #! /usr/bin/env bash
+
+ datasets='/vol2/home/smithc/data-sets'
+
+ # Use BEDtools intersect to identify the size of the largest overlap
+ # between CTCF and H3K4me3 locations. 
+ CTCF="$datasets/bedtools/ctcf.hela.chr22.bg.gz"
+ H3K4me3="$datasets/bed/encode.h3k4me3.hela.chr22.bed.gz"
+
+answer_1=$(bedtools intersect -wo -a $CTCF -b $H3K4me3 \
+    | sort -k15nr \
+    | head -n1 \
+    | awk '{print $15}')
+
+echo "answer-1: $answer_1"
+
+#Use BEDtools to calculate the GC content of nucleotides 19,000,000 to
+#19,000,500 on chr22 of hg19 genome build. Report the GC content as a
+#fraction (e.g., 0.50). 
+fasta="$datasets/fasta/hg19.chr22.fa"
+interval="$datasets/bed/interval.bed"
+
+answer_2=$(bedtools nuc -fi $fasta -bed $interval \
+    | grep -v '^#' \
+    | awk '{print $5}')
+
+echo "answer-2: $answer_2"
+
+#Use BEDtools to identify the length of the CTCF ChIP-seq peak (i.e.,
+#interval) that has the largest mean signal in ctcf.hela.chr22.bg.gz.
+encode="$datasets/bed/encode.tfbs.chr22.bed.gz"
+
+answer_3=$(bedtools map -c 4 -o mean -a $encode -b $CTCF \
+    | sort -k5nr \
+    | awk 'BEGIN {OFS="\t"} {print $0, $3 - $2}' \
+    | head -n1 \
+    | awk '{print $6}')
+
+echo "answer-3: $answer_3"
+
+#Use BEDtools to identify the gene promoter (defined as 1000 bp upstream
+#of a TSS) with the highest median signal in ctcf.hela.chr22.bg.gz. Report
+#the gene name (e.g., 'ABC123').
+TSS="$datasets/bed/tss.hg19.chr22.bed.gz"
+
+answer_4=$(bedtools map -c 4 -o median -a $TSS -b $CTCF \
+    | sort -k7nr \
+    | head -n1 \
+    | awk '{print $4}')
+
+echo "answer-4: $answer_4"
+
+#Use BEDtools to identify the longest interval on chr22 that is not
+#covered by genes.hg19.bed.gz. Report the interval like chr1:100-500.
+genes=$datasets/bed/genes.hg19.bed.gz
+
+answer_5=$(bedtools intersect -v -a $genes -b $CTCF \
+    | awk 'BEGIN {OFS="\t"} {print $0, $3 -$2}' \
+    | sort -k7nr \
+    | head -n1 \
+    | awk '{print $1":"$2"-"$3}')
+
+echo "answer-5: $answer_5"
+
+
+
+
+
